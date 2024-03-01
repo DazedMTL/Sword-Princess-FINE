8dc18e7b3c2f673f0fd702616f51b9208c81f38c
Dazed <dazedanon@gmail.com>
2024-03-01 14:09:05 -0600

    Make font bigger

--- Scripts/Window_Base.rb
+++ Scripts/Window_Base.rb
@@ -215,7 +215,7 @@ class Window_Base < Window
   #--------------------------------------------------------------------------
   def reset_font_settings
     change_color(normal_color)
-    contents.font.size = 18
+    contents.font.size = 22
     contents.font.bold = false
     contents.font.italic = false
   end
